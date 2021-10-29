0d4e77b56891235616004994738ecc971052b7c0
sivaram <sivaramsiva16@gmail.com>
2021-10-29 23:45:11 +0530

    Hackerrank problem solving

---
+++ Practice/hackerrank/hackerrank_in_string.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'hackerrankInString' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string hackerrankInString(string s) {
+     vector<char> word = {'k', 'n', 'a', 'r', 'r', 'e', 'k', 'c', 'a', 'h'};
+    for (int i = 0; i < s.length(); i++) {
+        if (word.size() == 0)
+            break;
+        else if (s[i] == word[word.size() - 1])
+            word.pop_back();
+    }
+    
+    if (word.size() == 0)
+        return "YES";
+    else
+        return "NO";
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = hackerrankInString(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
---
+++ Practice/hackerrank/pangrams.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+string pangrams(string s) {
+    int count = 0;
+    transform(s.begin(),
+              s.end(),
+              s.begin(),
+              ::toupper);
+    sort(s.begin(), s.end());
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] != s[i + 1])
+            count++;
+    }
+    if (count == 27)
+        return "pangram";
+    else
+        return "not pangram";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    string result = pangrams(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
